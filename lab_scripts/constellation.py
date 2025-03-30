171f8a6c33ea690870c2f297788e850194685b1a
Dylan Jones <ddj123@uw.edu>
2025-03-30 14:05:03 -0700

    updated constellation.py

--- lab_scripts/constellation.py
+++ lab_scripts/constellation.py
@@ -30,7 +30,27 @@ def generate_bit_map(symbols):
 class ConstellationPlot:
     def __init__(self, complex_train):
         self._complex_symbols = complex_train
-        self.bit_map = generate_bit_map(self._complex_symbols)
+        self.symbols_to_bit_map = self.generate_bit_map(self._complex_symbols)
+
+        self.bits_to_symbols_map = {v : k for k, v in self.symbols_to_bit_map.items()}
+
+    def generate_bit_map(self, symbols):
+        num_symbols = len(symbols)
+        modulation_order = np.log2(num_symbols)
+        def build_bits(order):
+            '''
+            Builds bit string combinations recursively
+            '''
+            if order == 0:
+                return [""]
+            else:
+                sub_combinations = build_bits(order - 1)
+                return ["1" + c for c in sub_combinations] + ["0" + c for c in sub_combinations]
+            
+        bits = build_bits(modulation_order)
+        bit_mapping = dict(zip(symbols, bits))
+        return bit_mapping
+    
 
     def visualize(self):
         reals = np.real(self._complex_symbols)
@@ -53,18 +73,17 @@ class ConstellationPlot:
         plt.show()
 
     def create_training_data_Ts(self, num_symbols, random=True):
-        bit_strings = np.array(list(self.bit_map.values()))
+        bit_strings = np.array(list(self.symbols_to_bit_map.values()))
         if random:
             random_indices = np.random.choice(
                 len(self._complex_symbols), size=num_symbols)
             random_symbols = self._complex_symbols[random_indices]
-            print(random_indices)
             random_bits = bit_strings[random_indices]
             return random_symbols, random_bits
         else:
             return np.resize(self._complex_symbols, num_symbols), np.resize(bit_strings, num_symbols)
-        
 
+    
 '''
 Functions that can be called in LabView
 '''
