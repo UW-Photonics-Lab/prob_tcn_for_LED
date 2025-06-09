hopefully fixed backprop mistake with encoder

lab_scripts/encoder_decoder.py
@@ -218,6 +218,8 @@ STATE['decoder'] = decoder
 STATE['optimizer'] = optimizer
 STATE['scheduler'] = scheduler
 
+MODELS = ['freq_encoder', "time_encoder", "decoder"]
+
 
 def evm_loss_func(true_symbols, predicted_symbols):
     """
@@ -254,6 +256,7 @@ def run_freq_encoder(real, imag, f_low, f_high, subcarrier_spacing):
     out = STATE['freq_encoder'](x, freqs)
     STATE['freq_encoder_in'] = x[:, k_min:]
     STATE['freq_encoder_out'] = out[:, k_min:] # Remove 0 frequency carriers
+    STATE['encoder_out'] = out[:, k_min:]
     STATE['frequencies'] = data_freqs
     # Store out in STATE to preserve computational graph
 
@@ -273,6 +276,8 @@ def run_time_encoder(real_x_t):
 
     # Now create encoder_out to backprop through pseudochannel
     STATE['encoder_out'] = STATE['time_decoder_out']
+    if STATE['encoder_out'].requires_grad == False:
+        print("Encoder out doesn't have grad!")
     return out.detach().cpu().contiguous().numpy().tolist()
 
 def differentiable_channel(encoder_out: torch.tensor, decoder_in: torch.tensor):
@@ -290,7 +295,9 @@ def differentiable_channel(encoder_out: torch.tensor, decoder_in: torch.tensor):
             return torch.fft.fft(x_flat.reshape(Nt, -1), axis=1)[:Nf]
         return H
     else:
-        return lambda x: x * (decoder_in / encoder_out) # Divide equal sized tensors to get H(jw)
+        def H(x):
+            x * (decoder_in / encoder_out) # Divide equal sized tensors to get H(jw)
+        return H
 
 def run_decoder(real, imag):
     # --- Reshape input to [Nt, Nf] ---
@@ -321,7 +328,7 @@ def update_weights(batch_size=config.batch_size) -> bool:
     '''
     output = False
     STATE['cycle_count'] += 1
-    true_symbols = STATE['encoder_in']
+    true_symbols = STATE['freq_encoder_in']
     predicted_symbols = STATE['decoder_out']
     evm_loss = evm_loss_func(true_symbols, predicted_symbols)
     if evm_loss is not None:
@@ -384,7 +391,7 @@ def update_weights(batch_size=config.batch_size) -> bool:
         wandb.log({"train/lr": lr}, step=step)
         wandb.log({"perf/frame_BER": STATE['frame_BER']}, step=step)
 
-        for model_name in ['encoder', 'decoder']:
+        for model_name in MODELS:
             model = STATE[model_name]
             for name, param in model.named_parameters():
                 # Log weights
@@ -406,7 +413,7 @@ def update_weights(batch_size=config.batch_size) -> bool:
         model_save_dir = os.path.join(script_dir, "..", "saved_models")
         os.makedirs(model_save_dir, exist_ok=True)
 
-        for model_name in ['encoder', 'decoder']:
+        for model_name in MODELS:
             model = STATE[model_name]
             save_path = os.path.join(model_save_dir, f"{model_name}_step{STATE['cycle_count']}.pt")
             torch.save(model.state_dict(), save_path)
@@ -421,12 +428,15 @@ def stop_training():
         # Save models
         model_save_dir = os.path.join(script_dir, "..", "saved_models")
         os.makedirs(model_save_dir, exist_ok=True)
-        encoder_path = os.path.join(model_save_dir, "encoder.pt")
+        freq_encoder_path = os.path.join(model_save_dir, "freq_encoder.pt")
+        time_encoder_path = os.path.join(model_save_dir, "time_encoder.pt")
         decoder_path = os.path.join(model_save_dir, "decoder.pt")
-        torch.save(STATE['encoder'].state_dict(), encoder_path)
+        torch.save(STATE['freq_encoder'].state_dict(), freq_encoder_path)
+        torch.save(STATE['time_encoder'].state_dict(), time_encoder_path)
         torch.save(STATE['decoder'].state_dict(), decoder_path)
         artifact = wandb.Artifact("transformer-models", type="model")
-        artifact.add_file(encoder_path)
+        artifact.add_file(freq_encoder_path)
+        artifact.add_file(time_encoder_path)
         artifact.add_file(decoder_path)
         wandb.log_artifact(artifact)
         wandb.finish()
@@ -444,8 +454,8 @@ def log_constellation(step, freqs=None, evm_loss=-99):
 
     try:
         # Extract data from STATE
-        enc_in = STATE['encoder_in']
-        enc_out = STATE['encoder_out']
+        enc_in = STATE['freq_encoder_in']
+        enc_out = STATE['freq_encoder_out']
         dec_in = STATE['decoder_in']
         dec_out = STATE['decoder_out']
 
@@ -481,7 +491,6 @@ def log_constellation(step, freqs=None, evm_loss=-99):
             ax.set_xlabel("Re")
             ax.set_ylabel("Im")
             ax.grid(True)
-            ax.legend()
 
         # Add a single shared colorbar if using frequency coloring
         if freqs is not None:
@@ -496,6 +505,7 @@ def log_constellation(step, freqs=None, evm_loss=-99):
         fig.savefig(plot_path, dpi=150)
         wandb.log({"Constellation Diagram": wandb.Image(plot_path)}, step=step)
         os.remove(plot_path)
+        plt.close(fig)
 
     except Exception as e:
         print(f"Failed to plot constellation at step {step}: {e}")

lab_scripts/modulate_OFDM_python.py
@@ -17,7 +17,7 @@ import torch
 # Get logging
 from lab_scripts.logging_code import *
 
-decode_logger = setup_logger(log_file=r"C:\Users\Public_Testing\Desktop\peled_interconnect\mldrivenpeled\debug_logs\decode_bits_log.txt")
+# decode_logger = setup_logger(log_file=r"C:\Users\Public_Testing\Desktop\peled_interconnect\mldrivenpeled\debug_logs\decode_bits_log.txt")
 
 
 def get_constellation(mode: str):
@@ -102,9 +102,9 @@ def modulate_data_OFDM(mode: str,
     return encoded_symbol_frame_real, encoded_symbol_frame_imag, grouped_true_bits_list, int(N_data)
 
 
-# AWG_MEMORY_LENGTH = 16384
+AWG_MEMORY_LENGTH = 16384
 # AWG_MEMORY_LENGTH = 105
-AWG_MEMORY_LENGTH = 65536
+# AWG_MEMORY_LENGTH = 65536
 
 BARKER_LENGTH = int(0.01 * (AWG_MEMORY_LENGTH)) if int(0.01 * (AWG_MEMORY_LENGTH)) > 5 else 5
 CP_RATIO = 0
@@ -181,7 +181,7 @@ def demodulate_OFDM_one_symbol_frame(y_t:list,
                                      Nt: int) -> list:
     '''Converts received y(t) into a bit string with optional debugging plots'''
 
-    debug_plots = True
+    debug_plots = False
 
     # Define paths for saving logs and plots
     log_dir = r'C:\Users\Public_Testing\Desktop\peled_interconnect\mldrivenpeled\debug_logs'
@@ -317,7 +317,7 @@ def demodulate_OFDM_one_symbol_frame(y_t:list,
 
         # Step 3: Apply FFT to each symbol and stack into a matrix
         Y_s_matrix = np.array([np.fft.fft(s) for s in symbols])
-        decode_logger.debug(f"Y_s Matrix Shsape: {Y_s_matrix.shape}\n")
+        # decode_logger.debug(f"Y_s Matrix Shsape: {Y_s_matrix.shape}\n")
 
 
         if debug_plots:
@@ -332,7 +332,7 @@ def demodulate_OFDM_one_symbol_frame(y_t:list,
 
         # # Extract both positive and negative frequency carriers
         num_data_carriers = N_data
-        decode_logger.debug(f"Number of Carriers: {N_data}\n")
+        # decode_logger.debug(f"Number of Carriers: {N_data}\n")
         # negative_carriers = Y_s[k_min + 1:num_data_carriers + k_min + 1]
         # positive_carriers = Y_s[-num_data_carriers -k_min - 1: -k_min -1]
         # data_subcarriers = negative_carriers
@@ -353,7 +353,7 @@ def demodulate_OFDM_one_symbol_frame(y_t:list,
 
         for symbol_fft in Y_s_matrix:
             negative_carriers = symbol_fft[k_min + 1:N_data + k_min + 1]
-            decode_logger.debug(f"Number of Received Carriers: {len(negative_carriers)}\n")
+            # decode_logger.debug(f"Number of Received Carriers: {len(negative_carriers)}\n")
             # Optionally include positive_carriers if your OFDM symbol is Hermitian symmetric
             # positive_carriers = symbol_fft[-num_data_carriers - k_min - 1: -k_min - 1]
 
@@ -368,7 +368,7 @@ def demodulate_OFDM_one_symbol_frame(y_t:list,
 
 
         data_subcarriers = np.concatenate(normalized_data_subcarriers)
-        decode_logger.debug(f"Number Total Carriers: {len(data_subcarriers)}\n")
+        # decode_logger.debug(f"Number Total Carriers: {len(data_subcarriers)}\n")
 
         if debug_plots:
             # Plot 6: Constellation Diagram